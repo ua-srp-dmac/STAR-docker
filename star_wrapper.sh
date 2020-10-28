f90a9a348d662d1662e45ab128732e3b7f8a9064
Michelle Yung <myung@email.arizona.edu>
2020-10-28 14:16:25 -0700

    update wrapper script for STAR

---
+++ star_wrapper.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+usage() {
+    echo ""
+    echo "Usage : sh $0 -g GENOME_DIR -f FASTQ_FILES -t NUM_THREADS -c READ_FILES_COMMAND"
+    echo ""
+
+cat <<'EOF'
+  -f </path/to/fastq file>
+  -a </path/to/reference genome>
+EOF
+    exit 0
+}
+
+while getopts "f:p:t:hg:r:" opt; do
+  case ${opt} in
+    f)
+      if [ -z "$read_files" ]
+      then
+        read_files=$OPTARG
+      else
+        read_files=$read_files','$OPTARG
+      fi
+      ;;
+    p)
+      if [ -z "$paired_files" ]
+      then
+        paired_files=$OPTARG
+      else
+        paired_files=$paired_files','$OPTARG
+      fi
+      ;;
+    t)
+      num_threads=$OPTARG
+      ;;
+    g)
+      genome_dir=$OPTARG
+      ;;
+    h)
+      usage
+      exit 1
+      ;;    
+    r)
+      read_files_command=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+echo "$read_files"
+echo "$paired_files"
+
+command="STAR --quantMode GeneCounts --genomeDir $genome_dir --readFilesIn $read_files"
+
+if [ -n "$paired_files" ]
+then
+  command+=" $paired_files"
+fi
+
+if [ -n "$read_files_command" ]
+then
+  command+=" --readFilesCommand $read_files_command"
+fi
+
+if [ -n "$num_threads" ]
+then
+  command+=" --runThreadN $num_threads"
+fi
+
+echo "$command"
+
+eval $command
